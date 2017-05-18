bf121d446701802cf6f2f7d1ef5e3b390e4d1332
AlexJenter <jenteralex@gmail.com>
2017-05-18 02:47:23 +0200

    initialise masonry

--- web/public/Packages/Sites/schule.signalwerkCh/Resources/Private/JavaScript/signalwerk.js
+++ web/public/Packages/Sites/schule.signalwerkCh/Resources/Private/JavaScript/signalwerk.js
@@ -2,6 +2,16 @@
 (function($) {
   $(function() {
 
+    var $grid = $('.neos-contentcollection').masonry({
+      // options
+      itemSelector: '.schule-signalwerkch-newsitem',
+      columnWidth: '.news-item__sizer',
+      percentPosition: true
+    });
+
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
+    });
 
   });
 })(jQuery);
