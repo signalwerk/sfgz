2f85ecce7271ca471c40c4d8aa44427ba051adb2
signalwerk <sh@signalwerk.ch>
2017-07-04 14:05:23 +0200

    ADD: New Addresses handling with category

---
+++ web/public/Packages/Sites/schule.signalwerkCh/Classes/schule/signalwerkCh/FlowQuery/FilterPropertiesFromArrayOperation.php
@@ -0,0 +1,70 @@
+<?php
+namespace schule\signalwerkCh\FlowQuery;
+
+use Neos\Eel\FlowQuery\FlowQuery;
+
+use Neos\Eel\FlowQuery\Operations\Object\FilterOperation;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\ContentRepository\Domain\Model\Node;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+
+
+
+class FilterPropertiesFromArrayOperation extends FilterOperation {
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var string
+     */
+    static protected $shortName = 'filterPropertiesFromArray';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param \Neos\Eel\FlowQuery\FlowQuery $flowQuery the FlowQuery object
+     * @param array $arguments the filter expression to use (in index 0)
+     * @return void
+     * @throws \Neos\Eel\FlowQuery\FizzleException
+     */
+    public function evaluate(\Neos\Eel\FlowQuery\FlowQuery $flowQuery, array $arguments)
+    {
+        if (!isset($arguments[0]) || empty($arguments[0])) {
+            return;
+        }
+        if (!is_string($arguments[0])) {
+            throw new \Neos\Eel\FlowQuery\FizzleException('filter operation expects string argument', 1332489625);
+        }
+        $filter = $arguments[0];
+
+        $propertyName = $arguments[1];
+
+
+        $parsedFilter = \Neos\Eel\FlowQuery\FizzleParser::parseFilterGroup($filter);
+
+        $filteredContext = array();
+        $context = $flowQuery->getContext();
+        foreach ($context as $element) {
+            $elementsFromProperty = $element->getProperty($propertyName);
+            print_r('-1-----');
+            print_r($filter);
+            //print_r($element);
+            print_r('-2-----');
+            print_r($propertyName);
+            print_r('-3-----');
+            print_r($elementsFromProperty);
+            print_r("-4-----\n");
+
+            foreach ($elementsFromProperty as $propertyElement) {
+                if ($this->matchesFilterGroup($propertyElement, $parsedFilter)) {
+                    $filteredContext[] = $element;
+                    break;
+                }
+            }
+
+        }
+        $flowQuery->setContext($filteredContext);
+    }
+
+}
---
+++ web/public/Packages/Sites/schule.signalwerkCh/Classes/schule/signalwerkCh/FlowQuery/RandomElement.php
@@ -0,0 +1,29 @@
+<?php
+namespace schule\signalwerkCh\FlowQuery;
+
+use Neos\Eel\FlowQuery\FlowQuery;
+use Neos\Eel\FlowQuery\Operations\AbstractOperation;
+
+class RandomElement extends AbstractOperation {
+
+        /**
+         * {@inheritdoc}
+         *
+         * @var string
+         */
+        static protected $shortName = 'randomElement';
+
+        /**
+         * {@inheritdoc}
+         *
+         * @param FlowQuery $flowQuery the FlowQuery object
+         * @param array $arguments the arguments for this operation
+         * @return void
+         */
+        public function evaluate(FlowQuery $flowQuery, array $arguments) {
+                $context = $flowQuery->getContext();
+                $randomKey = array_rand($context);
+                $result = array($context[$randomKey]);
+                $flowQuery->setContext($result);
+        }
+}
