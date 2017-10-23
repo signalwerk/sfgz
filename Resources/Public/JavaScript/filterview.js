17f5ab13e69b66062dd5b2825a2b251471a7a91e
signalwerk <sh@signalwerk.ch>
2017-10-23 15:16:11 +0200

    ADD: filter handling for contact and news

---
+++ Resources/Public/JavaScript/filterview.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Wait for the DOM to be ready
+$(function () {
+
+  // form restore
+  $('#filter-form').submit(function (event) {
+
+    var submitBtn = $("#filter-form-submit");
+    // submitBtn.attr('disabled', 'disabled');
+    // console.log('submitBtn', submitBtn);
+    submitBtn.addClass('button-submitting');
+
+    $('radio').bind('click', function () {
+      $(this).removeAttr('checked');
+    });
+
+    $('.noResult').addClass('hidden');
+
+    var cat = [];
+    $('.filterCat').each(function (index) {
+      var val = $('input[name=' + $(this).attr('name') + ']:checked').val();
+      if (val !== '*') {
+        cat.push(val);
+      }
+    });
+
+    console.log('--- CAT:', cat.join(','));
+
+    console.log('--- AJAX:', $('.ajax-list').data('list'));
+
+    // Assign handlers immediately after making the request,
+    // and remember the jqXHR object for this request
+    $.ajax({
+      url: '/',
+      data: {
+        nodeName: $('.ajax-list').data('list'),
+        cat: cat.join(',')
+      }
+    }).done(function (response) {
+      // .unwrap()
+      $('.ajax-list').html(response).children().children().unwrap();
+    }).fail(function () {
+      console.log("error");
+    }).always(function () {
+      // console.log( "complete" );
+      submitBtn.removeClass('button-submitting');
+    });
+
+    return false;
+  });
+});
+//# sourceMappingURL=filterview.js.map
